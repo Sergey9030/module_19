af5e2653ad4abe1c0db481ad37f6c400f058733c
Sergey9030 <709030@mail.ru>
2025-01-18 15:27:55 +0300

    Add files via upload
    Домашнее задание по теме "Сайт администрирования"

--- Shop/task1/admin.py
+++ Shop/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Buyer, Game
+
 
 # Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age') #Поля для отображения в списке
+    search_fields = ('name',) #Поле для поиска
+    list_filter = ('balance', 'age') #Поля для фильтрации
+    list_per_page = 30 #Количество записей на странице
+    readonly_fields = ('balance',) #Поле только для чтения
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')  # Поля для отображения в списке
+    search_fields = ('title',)  # Поле для поиска
+    list_filter = ('size', 'cost')  # Поля для фильтрации
+    list_per_page = 20  # Количество записей на странице
